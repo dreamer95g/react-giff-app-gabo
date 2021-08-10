f7356939c2845f6f464aeb0152db6166d53955e5
dreamer95g <dreamer95g@gmail.com>
2021-08-10 19:11:03 -0400

    First Commit

---
+++ docs/precache-manifest.86ea9411d5d8a3f606a2ffd8d903cd06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a183e7a80c72fe911457e746acaf3f15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "664f66c08fcfbc7fe010",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "664f66c08fcfbc7fe010",
+    "url": "/static/js/main.3e78b1c2.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
